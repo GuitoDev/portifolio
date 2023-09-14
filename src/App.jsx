e2b51969d15c4df1e344e06a81a33c7fbe922d89
GuilhermeAlvees <duxg.dev@gmail.com>
2023-09-13 22:41:34 -0300

    fazendo Sobre

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { fab } from "@fortawesome/free-brands-svg-icons";
 import { faCheckSquare, faCoffee } from "@fortawesome/free-solid-svg-icons";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import Sobre from "./components/Sobre";
+import Footer from "./components/Footer";
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 library.add(fab, faCheckSquare, faCoffee);
@@ -13,6 +15,8 @@ function App() {
     <>
       <Navbar />
       <Home />
+      <Sobre />
+      <Footer />
     </>
   );
 }
