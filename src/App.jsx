786c9d90971f668e5b35ec3392c3dfafb1ef8c4d
GuilhermeAlvees <duxg.dev@gmail.com>
2023-09-15 23:42:58 -0300

    responsividade

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,8 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 import { faCheckSquare, faCoffee } from "@fortawesome/free-solid-svg-icons";
 import Home from "./components/Home";
-import Navbar from "./components/Navbar";
-import Sobre from "./components/Sobre";
 import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 library.add(fab, faCheckSquare, faCoffee);
@@ -15,7 +14,6 @@ function App() {
     <>
       <Navbar />
       <Home />
-      <Sobre />
       <Footer />
     </>
   );
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,29 +1,45 @@
 import "./Navbar.css";
+import "animate.css";
 
 function Navbar() {
   return (
     <>
-     <header>
-         <nav className="navbar">
-        <div className="logo">
-          <img src="public\images\logo.png" alt="Logo" />
-        </div>
-        <ul className="nav-links">
-          <li>
-            <a href="#">Home</a>
-          </li>
-          <li>
-            <a href="#">Sobre</a>
-          </li>
-          <li>
-            <a href="#">Projetos</a>
-          </li>
-          <li>
-            <a href="#">Contato</a>
-          </li>
-        </ul>
-      </nav>
-     </header>
+      {/* Navbar Desktop*/}
+      <header>
+        <nav className="navbar animate__animated animate__fadeInDown">
+          <div className="logo">
+            <a href="/">
+              <img
+                src="public\images\logo.png"
+                alt="Logo"
+                width={70}
+                height={70}
+              />
+            </a>
+          </div>
+          <ul className="nav-links">
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="#">Sobre</a>
+            </li>
+            <li>
+              <a href="#">Projetos</a>
+            </li>
+            <li>
+              <a href="#">Contato</a>
+            </li>
+          </ul>
+
+          <label className="switch">
+            <input type="checkbox" />
+            <span className="slider"></span>
+          </label>
+        </nav>
+
+        {/* Versao Mobile */}
+      </header>
     </>
   );
 }
